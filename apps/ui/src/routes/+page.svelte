b9021f1d9cdd0fb76fe8bfd520c3d1d5b708fbae
Saurav Sahu <mrsauravsahu@outlook.com>
2022-10-30 13:30:14 +0530

    add hamburger menu

--- apps/ui/src/routes/+page.svelte
+++ apps/ui/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Icon from "svelte-awesome/components/Icon.svelte";
+  import { faBars } from "@fortawesome/free-solid-svg-icons";
   import Timeline from "$lib/timeline.svelte";
 
   /** @type {import('./$types').PageData} */
@@ -8,6 +10,11 @@
 </script>
 
 <nav>
+  <Icon
+    data={faBars}
+    scale={1.5}
+    style="color: white; padding: 0.5rem 1rem; cursor: pointer;"
+  />
   <h1>payobills</h1>
 </nav>
 <main>
@@ -19,7 +26,7 @@
   h1 {
     font-size: 1.5rem;
     color: white;
-    padding: 0.75rem 1rem;
+    padding: 0.75rem 1rem 0.75rem 0;
     font-weight: 400;
     margin: 0;
   }
@@ -32,6 +39,8 @@
 
   nav {
     background-color: #5b81bb;
+    display: flex;
+    align-items: center;
     margin: 0;
     padding: 0;
   }
