ac4cdb2e87d77a67fcb33b9d8ae4cb0db61857bb
Saurav Sahu <mrsauravsahu@outlook.com>
2022-10-30 12:23:54 +0530

    tweak timeline colors

--- apps/ui/src/lib/timeline.svelte
+++ apps/ui/src/lib/timeline.svelte
@@ -10,18 +10,44 @@
   <div class="items">
     {#each items as item}
       <div class="item">
-        <div class='pill'>
-        {#if item.billingDate > item.payByDate}
-        <span data-start={0} style={`flex-grow: ${item.payByDate}`} class='pill--filled'></span>
-        <span data-start={item.payByDate} style={`flex-grow: ${item.billingDate - item.payByDate + 1}`} class='pill-empty'></span>
-        <span data-start={item.billingDate} style={`flex-grow: ${31 - item.billingDate + 1}`} class='pill--filled'></span>
-        {:else}
-        <span data-start={0} style={`flex-grow: ${item.payByDate}`} class='pill-empty'></span>
-        <span data-start={item.payByDate} style={`flex-grow: ${ item.payByDate -item.billingDate + 1}`} class='pill--filled'></span>
-        <span data-start={item.billingDate} style={`flex-grow: ${31 - item.billingDate + 1}`} class='pill-empty'></span>
-      {/if}  
-      </div>
-        <span data-start={0} data-length={item.payByDate} class='item-title'>{item.name}</span>
+        <div class="pill">
+          {#if item.billingDate > item.payByDate}
+            <span
+              data-start={0}
+              style={`flex-grow: ${item.payByDate}`}
+              class="pill--filled"
+            />
+            <span
+              data-start={item.payByDate}
+              style={`flex-grow: ${item.billingDate - item.payByDate + 1}`}
+              class="pill--empty"
+            />
+            <span
+              data-start={item.billingDate}
+              style={`flex-grow: ${31 - item.billingDate + 1}`}
+              class="pill--filled"
+            />
+          {:else}
+            <span
+              data-start={0}
+              style={`flex-grow: ${item.payByDate}`}
+              class="pill--empty"
+            />
+            <span
+              data-start={item.payByDate}
+              style={`flex-grow: ${item.payByDate - item.billingDate + 1}`}
+              class="pill--filled"
+            />
+            <span
+              data-start={item.billingDate}
+              style={`flex-grow: ${31 - item.billingDate + 1}`}
+              class="pill--empty"
+            />
+          {/if}
+        </div>
+        <span data-start={0} data-length={item.payByDate} class="item-title"
+          >{item.name}</span
+        >
       </div>
     {/each}
   </div>
@@ -37,7 +63,7 @@
     flex-direction: column;
     justify-content: space-between;
     flex-grow: 1;
-    background-color: #d9d9d9;
+    background-color: #f3f3f3;
     padding: 1.5rem 1rem;
     border-radius: 0.5rem;
   }
@@ -54,13 +80,25 @@
     font-size: 0.75rem;
   }
 
-  .pill {display: flex; width: 100%; padding: 1rem 0 0.25rem 0;}
-  .pill > span {
-    height: 0.25rem;
-    border-radius: 0.25rem;
+  .pill {
+    display: flex;
+    width: 100%;
+    padding: 1rem 0 0.25rem 0;
   }
-  .pill--filled{background-color: #5b81bb;}
-  .item-title {font-size: 0.75rem;}
+  .pill > span {
+    height: 0.3125rem;
+    border-radius: 0.25rem;
+    margin: 1.5rem 0 0 0;
+  }
+  .pill--filled {
+    background-color: #7a98c5;
+  }
+  .pill--empty {
+    background-color: #e5ecf8;
+  }
+  .item-title {
+    font-size: 0.75rem;
+  }
   span {
     color: #9f9f9f;
   }
