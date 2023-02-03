51da86551b58b234620ab8b43dd2b7d4e3f159fd
Saurav Sahu <mrsauravsahu@outlook.com>
2023-02-04 00:11:52 +0530

    chore: locally deploy payments app (#29)

--- apps/payments/src/src/main.ts
+++ apps/payments/src/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 80, '0.0.0.0');
   console.log(`service running at ${await app.getUrl()}`);
 }
 bootstrap();
