d43f80f3ca1be20c611dce6500c7b2137249e42d
Saurav Sahu <mrsauravsahu@outlook.com>
2023-02-04 16:16:13 +0530

    update root route for payments app

--- apps/payments/src/src/app.controller.ts
+++ apps/payments/src/src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): { app: string } {
+    return { app: 'payments-service' }
   }
 }
