7d1cee9d60c05093d1b7e4651961e2d8f70e7903
Saurav Sahu <mrsauravsahu@outlook.com>
2023-04-16 18:40:11 +0530

    feat: add dapr support for payments ms

--- apps/payments/src/src/main.ts
+++ apps/payments/src/src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: false });
   await app.listen(process.env.PORT || 80, '0.0.0.0');
   console.log(`service running at ${await app.getUrl()}`);
 }
 bootstrap();
+
