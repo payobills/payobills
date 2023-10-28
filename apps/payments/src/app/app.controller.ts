6c6eeb236af763618771c487bde8bf87ff648299
Saurav Sahu <mrsauravsahu@outlook.com>
2023-10-29 01:26:29 +0530

    Feat(payments): User payments clean up with nestjs (#73)

--- apps/payments/src/app/app.controller.ts
+++ apps/payments/src/app/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: '',
+  version: 'v1'
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
---
+++ apps/payments/src/common/envelope.ts
@@ -0,0 +1,3 @@
+export interface Envelope<T> {
+    data: T;
+}
