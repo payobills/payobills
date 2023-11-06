07a7f4a3a8715bf51db6a3c7955fca5694bf629f
Saurav Sahu <mrsauravsahu@outlook.com>
2023-11-06 19:16:07 +0530

    Feat(UI): Login with OIDC (#76)
    * feat(ui): basic login with oidc on KeyCloak
    
    * improv: make timeline page protected behind login
    
    * improv: update login text
    
    * chore: update landing page
    
    * improv: inject OIDC_TENANT_URL from env vars

--- apps/ui/src/src/routes/+page.svelte
+++ apps/ui/src/src/routes/+page.svelte
@@ -1,32 +1,67 @@
 <script lang="ts">
-  import Timeline from "$lib/timeline.svelte";
-  import { goto } from "$app/navigation";
-  import Nav from "$lib/nav.svelte";
-  import {urql } from '$lib/stores/urql'
+    import { goto } from "$app/navigation";
 
-  import { queryStore, gql, getContextClient } from "@urql/svelte";
-
-  const billsQuery = queryStore({
-    client: $urql,
-    query: gql`
-      query {
-        bills {
-          name
-          billingDate
-          payByDate
-        }
-      }
-    `,
-  });
-
-  let vis;
+    async function login() {
+        await goto(
+            `${import.meta.env.VITE_OIDC_TENANT_URL}/auth/realms/homelab/protocol/openid-connect/auth?client_id=payobills&redirect_uri=http://localhost:3000/callback&response_type=code&grant_type=authorization_code&scope=openid`
+        );
+    }
 </script>
 
-{#if $billsQuery.fetching}
-  <p>Loading...</p>
-{:else if $billsQuery.error}
-  <p>🙆‍♂️ Uh oh! Unable to fetch your bills!</p>
-{:else}
-  <Timeline title="timeline view" items={$billsQuery.data.bills} />
-{/if}
+<div class="main">
+    <section class="intro">
+        <h1>payobills</h1>
+        <p>manage your finances like a pro</p>
+    </section>
+    <section class="cta">
+        <button on:click={login}>
+            <span>Login with SSO</span>
+            <img
+                src={`${import.meta.env.VITE_OIDC_TENANT_URL}/auth/welcome-content/keycloak-project.png`}
+                alt="Keycloak Logo"
+            />
+        </button>
+    </section>
+</div>
 
+<style>
+    .main {
+        align-self: flex-start;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        width: 100%;
+    }
+
+    .intro {
+        flex-grow: 2;
+    }
+
+    .cta {
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    img {
+        height: 2rem;
+        filter: grayscale();
+    }
+    span {
+        margin-right: 0.5rem;
+    }
+
+    button {
+        width: auto;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #202020;
+    }
+
+    h1 {
+        font-size: 4rem;
+    }
+</style>
