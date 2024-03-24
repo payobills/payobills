10dde875862d8a94590c8df5cd4da026adbe52ea
Example <mrsauravsahu@outlook.com>
2024-03-24 15:08:26 +0530

    enhanc: cleanup of NocoDB based bills APIs

--- apps/bills/src/Data.Contracts/Models/Bill.cs
+++ apps/bills/src/Data.Contracts/Models/Bill.cs
@@ -1,12 +1,14 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Payobills.Bills.Data.Contracts.Models;
 
 public class Bill
 {
     public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public int? BillingDate { get; set; } = null;
-    public int? PayByDate { get; set; } = null;
-    public int? LatePayByDate { get; set; } = null;
+    public int? BillingDate { get; set; }
+    public int? PayByDate { get; set; }
+    public int? LatePayByDate { get; set; }
     public List<BillPayment> Payments { get; set; } = new();
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
