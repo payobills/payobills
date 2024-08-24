b102f5c864d0aa7ccfad36a874dca5efcd9058c4
Example <mrsauravsahu@outlook.com>
2024-08-24 14:33:01 +0530

    feat(ui): render payment charts by month

--- apps/ui/src/src/routes/bills/+page.svelte
+++ apps/ui/src/src/routes/bills/+page.svelte
@@ -1,148 +1,251 @@
 <script lang="ts">
-    import { queryStore, gql } from "@urql/svelte";
-    import { urql } from "$lib/stores/urql";
-    import { page } from "$app/stores";
-    import PaymentTimelinePill from "$lib/payment-timeline-pill.svelte";
-    import Nav from "$lib/nav.svelte";
-    import Card from "$lib/card.svelte";
+  import { queryStore, gql } from "@urql/svelte";
+  import { urql } from "$lib/stores/urql";
+  import { page } from "$app/stores";
+  import PaymentTimelinePill from "$lib/payment-timeline-pill.svelte";
+  import Nav from "$lib/nav.svelte";
+  import Card from "$lib/card.svelte";
+  import { onMount } from "svelte";
 
-    let billId: any;
-    let billByIdQuery: any;
-    let refreshKey: number = Date.now();
+  let billId: any;
+  let billByIdQuery: any;
+  let refreshKey: number = Date.now();
 
-    billId = $page.url.searchParams.get("id");
-    $: billByIdQuery = queryStore({
-        client: $urql,
-        query: gql`
-            query billById($billId: String!) {
-                billById(id: $billId) {
-                    id
-                    name
-                    billingDate
-                    payByDate
-                    updatedAt
-                    createdAt
-                    payments {
-                        id
-                        amount
-                        billingPeriod {
-                            start
-                            end
-                        }
-                        createdAt
-                        updatedAt
-                    }
-                }
+  billId = $page.url.searchParams.get("id");
+  $: billByIdQuery = queryStore({
+    client: $urql,
+    query: gql`
+      query billById($billId: String!) {
+        billById(id: $billId) {
+          id
+          name
+          billingDate
+          payByDate
+          updatedAt
+          createdAt
+          payments {
+            id
+            amount
+            billingPeriod {
+              start
+              end
             }
+            createdAt
+            updatedAt
+          }
+        }
+      }
+    `,
+    variables: { billId, refreshKey },
+  });
+
+  async function markPaid() {
+    const markPaidQuery = $urql
+      .mutation(
+        gql`
+          mutation markPayment($billId: UUID!) {
+            markPayment(dto: { id: $billId }) {
+              id
+              createdAt
+              updatedAt
+              billingPeriod {
+                start
+                end
+              }
+            }
+          }
         `,
-        variables: { billId, refreshKey },
+        { billId }
+      )
+      .toPromise();
+
+    let { error } = await markPaidQuery;
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    refreshKey = Date.now();
+  }
+
+  let loaded = false;
+  let ApexCharts: any;
+
+  onMount(async () => {
+    if ((window as any).ApexCharts) {
+      loaded = true;
+      return;
+    }
+
+    await load();
+  });
+
+  const load = async () => {
+    const module = await import("apexcharts");
+    ApexCharts = module.default;
+    (window as any).ApexCharts = ApexCharts;
+  };
+
+  const chart = (node: any) => {
+    if (!loaded) load();
+
+    const orderedData  = $billByIdQuery.data.billById.payments.sort((a:any,b:any) => {
+        return  new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
     });
 
-    async function markPaid() {
-        const markPaidQuery = $urql
-            .mutation(
-                gql`
-                    mutation markPayment($billId: UUID!) {
-                        markPayment(dto: { id: $billId }) {
-                            id
-                            createdAt
-                            updatedAt
-                            billingPeriod {
-                                start
-                                end
-                            }
-                        }
-                    }
-                `,
-                { billId }
-            )
-            .toPromise();
 
-        let { error } = await markPaidQuery;
+    let allData = orderedData.map((p: any) => {
+      return {
+        x: Intl.DateTimeFormat(undefined, {
+          month: "short",
+          year: "2-digit",
+        }).format(new Date(p.createdAt).getTime()),
+        y: p.amount,
+        note: `${p.amount}`,
+      };
+    });
 
-        if (error) {
-            console.log(error);
-            return;
+    let data = allData.reduce(
+      (accumulator: any[], current: any, index: number) => {
+        if (index == 0) return [current];
+
+        if (current.x == accumulator[accumulator.length - 1].x) {
+          let last = accumulator[accumulator.length - 1];
+          return [
+            ...accumulator.slice(0, -1),
+            {
+              y: last.y + current.y,
+              x: last.x,
+              note: `${last.note} - + ${current.y}`
+            },
+          ];
         }
 
-        refreshKey = Date.now();
-    }
+        return [
+          ...accumulator,
+          {
+            x: current.x,
+            y: current.y,
+            note: current.note
+          },
+        ];
+      },
+      []
+    );
+
+    let options: any = {
+      legend: {
+        show: false,
+      },
+      stroke: {
+        curve: "smooth",
+        width: 2,
+      },
+      chart: {
+        type: "line",
+      },
+      series: [
+        {
+          name: "payments",
+          data: data.map((p: any) => p.y),
+        },
+      ],
+      xaxis: {
+        categories: data.map((p: any) => p.x),
+      },
+    };
+
+    let myChart = new (window as any).ApexCharts(node, options);
+    myChart.render();
+
+    return {
+      update(options: any) {
+        myChart.updateOptions(options);
+      },
+      destroy() {
+        myChart.destroy();
+      },
+    };
+  };
 </script>
 
 <Nav />
 <Card>
-    <div class="content">
-        {#if $billByIdQuery.fetching}
-            <p>Loading...</p>
-        {:else if $billByIdQuery.error}
-            <p>🙆‍♂️ Uh oh! Unable to fetch your bill!</p>
-        {:else}
-            <h1>{$billByIdQuery.data.billById.name}</h1>
-            <PaymentTimelinePill
-                item={$billByIdQuery.data.billById}
-                showLabel={false}
-            />
-            {#if $billByIdQuery.data.billById.payments.length == 0}
-                <p>we don't see any payments made for this bill. 😞</p>
-            {:else}
-                <h2>past payments</h2>
-                {#each $billByIdQuery.data.billById.payments as payment}
-                    <p>
-                        {#if payment.amount}
-                            <!-- TODO: Get currency from user, suggest based on Intl -->
-                            <span class="amount"
-                                >{new Intl.NumberFormat(undefined, { style: 'currency', currency: 'INR' }).format(
-                                    payment.amount,
-                                  )} (manually entered)</span
-                            >
-                        {:else}
-                            <span class="amount--unknown">Unknown Amount </span>
-                        {/if}
-                        <span>•</span>
-                        <span class="paid-on"
-                            >{Intl.DateTimeFormat(undefined, {
-                                month: "long",
-                            }).format(
-                                new Date(payment.createdAt).getTime()
-                            )}</span
-                        >
-                    </p>
-                {/each}
-            {/if}
-        {/if}
+  <div class="content">
+    {#if $billByIdQuery.fetching}
+      <p>Loading...</p>
+    {:else if $billByIdQuery.error}
+      <p>🙆‍♂️ Uh oh! Unable to fetch your bill!</p>
+    {:else}
+      <h1>{$billByIdQuery.data.billById.name}</h1>
+      <PaymentTimelinePill
+        item={$billByIdQuery.data.billById}
+        showLabel={false}
+      />
 
-        <div class="actions">
-            <button class="markPaid" on:click={async () => await markPaid()}
-                >mark as paid</button
+      {#if $billByIdQuery.data.billById.payments.length == 0}
+        <p>we don't see any payments made for this bill. 😞</p>
+      {:else}
+        <div use:chart></div>
+        <h2>past payments</h2>
+        {#each $billByIdQuery.data.billById.payments as payment}
+          <p>
+            {#if payment.amount}
+              <!-- TODO: Get currency from user, suggest based on Intl -->
+              <span class="amount"
+                >{new Intl.NumberFormat(undefined, {
+                  style: "currency",
+                  currency: "INR",
+                }).format(payment.amount)} (manually entered)</span
+              >
+            {:else}
+              <span class="amount--unknown">Unknown Amount</span>
+            {/if}
+            <span>•</span>
+            <span class="paid-on"
+              >{Intl.DateTimeFormat(undefined, {
+                month: "long",
+                year: "2-digit",
+              }).format(new Date(payment.createdAt).getTime())}</span
             >
-        </div>
+          </p>
+        {/each}
+      {/if}
+    {/if}
+
+    <div class="actions">
+      <button class="markPaid" on:click={async () => await markPaid()}
+        >mark as paid</button
+      >
     </div>
+  </div>
 </Card>
 
 <style>
-    h1 {
-        color: var(--primary-color);
-        font-size: 1.2rem;
-        font-weight: 600;
-    }
-    h2{
-        font-size: 1rem;
-    }
-    p {
-        font-size: .8rem;
-    }
-    .content {
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
-    }
-    .actions {
-        display: flex;
-        flex-grow: 1;
-        justify-content: flex-end;
-    }
+  h1 {
+    color: var(--primary-color);
+    font-size: 1.2rem;
+    font-weight: 600;
+  }
+  h2 {
+    font-size: 1rem;
+  }
+  p {
+    font-size: 0.8rem;
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+  .actions {
+    display: flex;
+    flex-grow: 1;
+    justify-content: flex-end;
+  }
 
-    .markPaid {
-        align-self: flex-end;
-    }
+  .markPaid {
+    align-self: flex-end;
+  }
 </style>
