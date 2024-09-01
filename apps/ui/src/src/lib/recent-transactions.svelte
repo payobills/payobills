a417b72e77ab0e779597fd7a22f4e00ad77f0991
Example <mrsauravsahu@outlook.com>
2024-09-01 16:35:15 +0530

    feat: add graph for recent transactions

--- apps/ui/src/src/lib/recent-transactions.svelte
+++ apps/ui/src/src/lib/recent-transactions.svelte
@@ -1,18 +1,140 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   export let transactions: any[] = [];
   export let showViewAllCTA = true;
   export let showAllTransactions = false;
+
+  let loaded = false;
+  $: apexCharts = null;
+
+  onMount(async () => {
+    if ((window as any).ApexCharts) {
+      loaded = true;
+      return;
+    }
+
+    await load();
+  });
+
+  const load = async () => {
+    const module = await import("apexcharts");
+    (window as any).ApexCharts = module.default;
+    apexCharts = module.default as any;
+    loaded = true;
+  };
+
+  const chart = (node: any) => {
+    if (!loaded) load();
+
+    const orderedData = transactions.sort((a: any, b: any) => {
+      return new Date(a.backDateString).getTime() - new Date(b.backDateString).getTime();
+    });
+
+    let allData = orderedData.map((p: any) => {
+      return {
+        x: Intl.DateTimeFormat(undefined, {
+          day: '2-digit',
+          month: "short",
+          year: "2-digit",
+        }).format(new Date(p.backDateString).getTime()),
+        y: p.amount,
+        note: `${p.amount}`,
+      };
+    });
+
+    let data = allData.reduce(
+      (accumulator: any[], current: any, index: number) => {
+        if (index == 0) return [current];
+
+        if (current.x == accumulator[accumulator.length - 1].x) {
+          let last = accumulator[accumulator.length - 1];
+          return [
+            ...accumulator.slice(0, -1),
+            {
+              y: last.y + current.y,
+              x: last.x,
+              note: `${last.note} - + ${current.y}`,
+            },
+          ];
+        }
+
+        return [
+          ...accumulator,
+          {
+            x: current.x,
+            y: current.y,
+            note: current.note,
+          },
+        ];
+      },
+      []
+    );
+
+    let options: any = {
+      colors: ["#7a98c5"],
+      legend: {
+        show: false,
+      },
+      stroke: {
+        curve: "smooth",
+        width: 2,
+      },
+      chart: {
+        type: "area",
+      },
+      series: [
+        {
+          name: "payments",
+          data: data.map((p: any) => p.y),
+        },
+      ],
+      xaxis: {
+        categories: data.map((p: any) => p.x),
+        labels:{show:false}
+      },
+      yaxis: {
+        labels: {
+          show: false,
+          formatter: (value: number) => `₹ ${value}`,
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        style: {
+          colors: ["#96b7e8"],
+        },
+        formatter: (value: number) => `₹ ${value}`,
+      },
+    };
+
+    let myChart = new (window as any).ApexCharts(node, options);
+    myChart.render();
+
+    return {
+      update(options: any) {
+        myChart.updateOptions(options);
+      },
+      destroy() {
+        myChart.destroy();
+      },
+    };
+  };
 </script>
 
 <div class="container">
   <div class="title">
     <h1>Recent Transactions</h1>
     {#if showViewAllCTA}
-    <a href="/transactions">view all</a>
+      <a href="/transactions">view all</a>
     {/if}
   </div>
 
-  {#each (showAllTransactions ? transactions : transactions.slice(0, 5)) as transaction}
+  {#if (showAllTransactions && ApexCharts)}
+    <div use:chart></div>
+  {/if}
+
+  {#each showAllTransactions ? transactions : transactions.slice(0, 5) as transaction}
     <div class="recent-transaction">
       <div>
         <span>{transaction.merchant}</span>
