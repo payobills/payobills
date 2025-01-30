dd78eb9cf40a28cbf77d0b60f0adec636b08aaa9
Sahu <mrsauravsahu@outlook.com>
2025-01-31 00:49:07 +0530

    improv: add intermediate URLs for transactions

--- apps/ui/src/src/routes/transactions/+page.svelte
+++ apps/ui/src/src/routes/transactions/+page.svelte
@@ -3,7 +3,7 @@
   import { onMount } from "svelte";
 
   onMount(() => {
-    const currentTransactionMonth = `transactions/${new Date().getUTCFullYear()}/${new Date().getMonth()}`;
+    const currentTransactionMonth = `/transactions/${new Date().getUTCFullYear()}/${new Date().getUTCMonth() + 1}`;
     goto(currentTransactionMonth);
   });
 </script>
