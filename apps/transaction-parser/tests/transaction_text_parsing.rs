9a56e8ab9b5d9ccf3edfbcce717884f17ad66c8d
Sahu <mrsauravsahu@outlook.com>
2025-03-05 01:31:18 +0530

    fix: edge cases for amex

--- apps/transaction-parser/tests/transaction_text_parsing.rs
+++ apps/transaction-parser/tests/transaction_text_parsing.rs
@@ -10,6 +10,8 @@ mod tests {
     #[test_case("Alert: You've spent INR 410.58 on your AMEX card ** 81009 at MERCHANT 1 on 3 March, 2025 at 04:44 PM IST. Call 18004190691 if this was not made by you.", "INR", "410.58", "81009", "MERCHANT 1", "3 March, 2025 at 04:44 PM", "IST"; "INR")]
     #[test_case("Alert: You've spent €60.00 on your AMEX card ** 81009 at MERCHANT 2 on 13 February, 2025 at 04:35 PM IST. Call 18004190691 if this was not made by you.", "€", "60.00", "81009", "MERCHANT 2", "13 February, 2025 at 04:35 PM", "IST"; "EUR")]
     #[test_case("Alert: You've spent €60.00 on your AMEX card ** 81009 on 13 February, 2025 at 04:35 PM IST. Call 18004190691 if this was not made by you.", "€", "60.00", "81009", "", "13 February, 2025 at 04:35 PM", "IST"; "EUR without merchant")]
+    #[test_case("Alert: You've spent €12.00 on your AMEX card ** 81009 at SNCF-VOYAGEU on 5 February, 2025 at 06:11 PM IST. Call 18004190691 if this was not made by you.", "€", "12.00", "81009", "SNCF-VOYAGEU", "5 February, 2025 at 06:11 PM", "IST"; "EUR merchant with dash")]
+    #[test_case("Alert: You've spent INR 1,00,000.00 on your AMEX card ** 81009 at MERCHANT 1 on 3 March, 2025 at 04:44 PM IST. Call 18004190691 if this was not made by you.", "INR", "1,00,000.00", "81009", "MERCHANT 1", "3 March, 2025 at 04:44 PM", "IST"; "Large amount")] 
     // #[test_case("when both operands are positive")]
     // #[test_case("when operands are swapped")]
     fn test_transaction_text_parsing(test_transaction_text: &str,
@@ -20,7 +22,7 @@ mod tests {
         date: &str,
         timezone: &str
         ) {
-        let regex_pattern = r"^Alert: You've spent (?P<currency>[^\d\s]+)\s{0,1}(?P<amount>\d+\.?\d+) on your AMEX card \*\* (?P<card>\d+)( at ){0,1}(?P<merchant>[a-zA-Z0-9\s]*) on (?P<date>[\d]{1,2} \w+, [\d]{4} at \d{2,2}:\d{2,2} [A-Z]{2,2}) (?P<timezone>[A-Z]{2,3})\..*$";
+        let regex_pattern = r"^Alert: You've spent (?P<currency>[^\d\s]+)\s{0,1}(?P<amount>[\d,]+\.?\d+) on your AMEX card \*\* (?P<card>\d+)( at ){0,1}(?P<merchant>.*) on (?P<date>[\d]{1,2} \w+, [\d]{4} at \d{2,2}:\d{2,2} [A-Z]{2,2}) (?P<timezone>[A-Z]{2,3})\..*$";
         let re = Regex::new(regex_pattern).unwrap();
         let caps = re.captures(test_transaction_text).unwrap();
 
