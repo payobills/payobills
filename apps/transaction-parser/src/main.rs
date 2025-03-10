9b2a1c3cb29def1cdf45be9cb03f8f1692091d58
Sahu <mrsauravsahu@outlook.com>
2025-03-10 22:14:30 +0530

    chore: update naming convention for env

--- apps/transaction-parser/src/main.rs
+++ apps/transaction-parser/src/main.rs
@@ -643,9 +643,9 @@ async fn main() {
         base_name_payobills: String::from("payobills"),
         table_name_payobills_transactions: String::from("transactions"), // std::env::var("NOCODB_TABLE_NAME").expect("NOCODB_TABLE_NAME must be set")
         base_name_currencies: std::env::var("NOCODB__BASE_NAME__CURRENCIES")
-            .expect("NOCODB__BASENAME_CURRENCIES must be set"),
-        table_name_currencies_historical: std::env::var("NOCODB_TABLE_NAME_CURRENCIES_HISTORICAL")
-            .expect("NOCODB__TABLE_NAME_CURRENCIES_HISTORICAL must be set"),
+            .expect("NOCODB__BASE_NAME__CURRENCIES must be set"),
+        table_name_currencies_historical: std::env::var("NOCODB__TABLE_NAME__CURRENCIES__HISTORICAL")
+            .expect("NOCODB__TABLE_NAME__CURRENCIES__HISTORICAL must be set"),
         api_key: std::env::var("NOCODB__INTEGRATION_TOKEN")
             .expect("NOCODB__INTEGRATION_TOKEN must be set"),
     };
