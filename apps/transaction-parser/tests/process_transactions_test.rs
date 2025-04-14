2ef63d3d282e3b4f01ce5012bd828a8adfcd52ec
Sahu <mrsauravsahu@outlook.com>
2025-04-15 03:25:23 +0530

    tests: add sanity test for process transactions test file

---
+++ apps/transaction-parser/tests/process_transactions_test.rs
@@ -0,0 +1,11 @@
+use regex::Regex;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    fn test_process_transactions_no_transactions() {
+        assert_eq!(1 + 1, 2);
+    }
+}
