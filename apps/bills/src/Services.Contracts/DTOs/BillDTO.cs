3c3dbe08866409664a5cb4145fc51cf525707a06
mrsauravsahu <mrsauravsahu@outlook.com>
2025-04-27 16:49:56 +0530

    chore: add back payments field to BillDTO

--- apps/bills/src/Services.Contracts/DTOs/BillDTO.cs
+++ apps/bills/src/Services.Contracts/DTOs/BillDTO.cs
@@ -12,6 +12,7 @@ public class BillDTO
     public int? BillingDate { get; set; }
     public int? PayByDate { get; set; }
     public int? LatePayByDate { get; set; }
+    public List<PaymentDTO> Payments { get; set; } = [];
     public bool IsEnabled { get; set; } = false;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
