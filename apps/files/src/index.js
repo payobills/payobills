a87a56b873db0d74df76ca68c914605e42359c8c
mrsauravsahu <mrsauravsahu@outlook.com>
2025-05-31 22:04:26 +0530

    feat: setup hello world apollo server

--- apps/files/src/index.js
+++ apps/files/src/index.js
@@ -1,6 +1,11 @@
 /// @ts-check
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@as-integrations/express5');
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
+const { buildSchema, printSchema } = require('graphql');
 const Express = require("express");
 const multer = require('multer');
+const http = require('http');
 
 const NocoDbClientFactory = require('./nocodb-client.factory');
 const MongoClientFactory = require("./mongo-client.factory");
@@ -23,12 +28,55 @@ async function main() {
     rabbitChannel
   };
 
+  const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+  // A map of functions which return data for the schema.
+  const resolvers = {
+    Query: {
+      hello: () => 'world',
+    },
+  };
+
+  const httpServer = http.createServer(app);
+
+  // Set up Apollo Server
+  const apolloServer = new ApolloServer({
+    csrfPrevention: false,
+    typeDefs,
+    resolvers,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      // ApolloServerPluginLandingPageDisabled()
+      //  ApolloServerPluginLandingPageLocalDefault()
+    ],
+  });
+
+  await apolloServer.start();
+
+  app.use(
+    '/graphql',
+    Express.json(),
+    /// @ts-ignore
+    expressMiddleware(apolloServer),
+  );
+
   app.get('/', (_, res) => (res.send({ app: 'files-service' })))
   /// @ts-ignore
   app.post('/files', upload.single('file'), postFile(DI))
-  app.listen(port, host, () => {
-    console.log(`App listening on port ${host}:${port}`)
-  })
+
+  app.get('/graphql-sdl', (_, res) => {
+    const schema = buildSchema(typeDefs);
+    const sdlString = printSchema(schema);
+    res.header('Content-Type', 'application/graphql+json');
+    res.send(sdlString);
+  });
+
+  console.log(`App listening on port ${host}:${port}`)
+  await new Promise((resolve) => httpServer.listen({ port, host, resolve }));
 }
 
 main().catch(console.error)
