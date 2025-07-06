c316bf09e90a561c870dd18404d4e45a01086d1d
mrsauravsahu <mrsauravsahu@outlook.com>
2025-07-06 13:48:02 +0530

    chore: update design for bill payment cards

--- apps/ui/src/src/lib/bills/bill-payment.svelte
+++ apps/ui/src/src/lib/bills/bill-payment.svelte
@@ -2,6 +2,7 @@
   import { goto } from "$app/navigation";
   import Card from "$lib/card.svelte";
   import { onMount } from "svelte";
+  import { withOrdinalSuffix } from "../../utils/ordinal-suffix";
 
   export let bill;
 
@@ -15,54 +16,120 @@
   $: {
     const diffInDays = bill.payByDate - todaysDay;
     if (diffInDays > 0)
-      billDueDetails = { string: `Due in ${diffInDays} days`, status: "due" };
+      billDueDetails = { string: `Due in ${diffInDays} days`, status: "due", l2Status: diffInDays <= 5 ? 'warning':'ok' };
     else if (diffInDays < 0)
       billDueDetails = {
         string: `Overdue by ${-diffInDays} days`,
-        status: "overdue",
+        status: "overdue"
       };
     else billDueDetails = { string: "Due today", status: "today" };
   }
 </script>
 
 <div class="container">
-  <div>{bill.name}</div>
-  <button
+  <div class="header">
+    <div class="name">{bill.name}</div>
+    <div class="actions">
+      <button
+        on:click={() => goto(`bills/${bill.id}`)}
+        >Pay now
+      </button>
+    </div>
+  </div>
+
+  <hr />
+  <div class="card-item">
+    Bill generates <strong>{withOrdinalSuffix(bill.billingDate)}</strong> of every
+    month
+  </div>
+  <div class="card-item">
+    Bill payment due <strong>{withOrdinalSuffix(bill.payByDate)}</strong> of every
+    month
+  </div>
+  <div class="card-item">
+    Current bill status <span class={`due-status due-status--${billDueDetails?.status} ${billDueDetails?.l2Status? `due-status--${billDueDetails?.status}--${billDueDetails?.l2Status}`: ''}`}><strong>{billDueDetails?.string}</strong></span>
+  </div>
+
+  <!-- <button
     on:click={() => goto(`bills/${bill.id}`)}
-    class={`bill due-status due-status--${billDueDetails?.status}`}
+    class={`due-status due-status--${billDueDetails?.status}`}
   >
-    {billDueDetails?.string}
-  </button>
+    
+  </button> -->
   <!-- <PaymentTimelinePill {item} /> -->
   <!-- <div>{JSON.stringify(bill)}</div> -->
 </div>
 
 <style>
-  .card-container {
-    border: 1px solid grey 0.25rem;
-    box-shadow: 0 3px 3px 1px rgba(0, 0, 0, 0.16);
+  .container {
+    background-color: rgb(233, 233, 233);
+    border-radius: 0.125rem;
+    border: 0.125rem solid rgb(216, 216, 216);
+    /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: top;
+    margin-top: 1rem;
+  }
+  
+  .container > div:nth-of-type(2) {
+    margin-top: 1rem;
   }
 
-  .container {
-    background-color: #e0e0e0;
-    padding: 1rem;
+  .container > div:last-of-type {
+    margin-bottom: 1rem;
+  }
+
+  .header {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    margin-bottom: .5rem;
   }
 
+  .name {
+    font-size: 1rem;
+    font-weight: 400;
+    margin: .5rem 1rem 0.5rem 1rem;
+  }
+
+  strong {
+    font-weight: 800;
+  }
+
+  .card-item {
+    margin: 0.5rem 1rem;
+    font-size: 0.75rem;
+  }
+
+  hr {
+    width: 100%;
+    height: 0.0625rem;
+    border: none;
+    background-color: rgb(216, 216, 216);
+    margin: 0;
+  }
+
+  button { margin: .5rem; padding: .5rem;}
+
   .due-status {
+    padding: .25rem;
     min-width: 40%;
     text-align: center;
     /* padding: 0.25rem; */
-    border-radius: 0.125rem;
+    border-radius: 0.55rem;
+    /* margin: 0.5rem 1rem 1rem 1rem; */
   }
 
   .due-status--due {
     color: white;
     background-color: var(--primary-accent-color);
+  }
+
+  .due-status--due--warning {
+    color: white;
+    background-color: rgb(225, 153, 20);
   }
 
   .due-status--overdue {
