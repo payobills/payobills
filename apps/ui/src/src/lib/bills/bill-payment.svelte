614c8a452b0679bcfa95141616c81d0292a130e0
mrsauravsahu <mrsauravsahu@outlook.com>
2025-07-07 15:07:33 +0530

    fix: border radius for bill payment card

--- apps/ui/src/src/lib/bills/bill-payment.svelte
+++ apps/ui/src/src/lib/bills/bill-payment.svelte
@@ -74,9 +74,8 @@
 <style>
   .container {
     background-color: rgb(233, 233, 233);
-    border-radius: 0.125rem;
+    border-radius: 0.425rem;
     border: 0.125rem solid rgb(216, 216, 216);
-    /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); */
     display: flex;
     flex-direction: column;
     justify-content: space-between;
