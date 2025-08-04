0bbddbaa21378558200805f1768dae61299dd551
mrsauravsahu <mrsauravsahu@outlook.com>
2025-08-05 02:08:27 +0530

    chore: update mode command name

--- apps/transaction-parser/src/main.rs
+++ apps/transaction-parser/src/main.rs
@@ -5,26 +5,36 @@ pub mod cli;
 pub mod payobills;
 
 #[derive(Debug, Clone, Parser)]
-#[command(version, about, long_about = None)]
+#[command(about, long_about = None)]
 struct Args {
-    #[arg(name = "sub_command")]
-    sub_command: String,
+    #[arg(
+        name = "mode",
+        default_value = "cli",
+        help = "Mode to run, either 'api' or 'cli'"
+    )]
+    mode: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = Args::parse();
 
-    if matches.sub_command == String::from("api") {
+    if matches.mode == String::from("api") {
         println!("Starting API server at 0.0.0.0:3000");
 
         let address = String::from("0.0.0.0:3000");
         return axum::Server::bind(&address.parse().unwrap())
-        .serve(crate::api::create_router().into_make_service())
-        .await
-        .map_err(|e| Box::new(e) as Box<dyn std::error::Error>);
+            .serve(crate::api::create_router().into_make_service())
+            .await
+            .map_err(|e| Box::new(e) as Box<dyn std::error::Error>);
+    } else if matches.mode == String::from("cli") {
+        return crate::cli::cli().await;
     } else {
-        return crate::cli::cli()
-        .await;
+        {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Unknown mode specified. Use 'api' or 'cli'.",
+            )));
+        }
     }
 }
