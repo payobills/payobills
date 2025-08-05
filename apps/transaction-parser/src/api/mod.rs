bfea43d866c96ff4340ab5f6e418a8fe77fa8512
Saurav Sahu <mrsauravsahu@outlook.com>
2025-08-05 18:17:40 +0530

    Improv(Transaction-Parser): Setup Deployable helm chart (#374)
    * chore: add files for transaction-parser helm chart
    
    * chore: clean BUILD file
    
    * chore: add health check route
    
    * chore: update env vars

--- apps/transaction-parser/src/api/mod.rs
+++ apps/transaction-parser/src/api/mod.rs
@@ -1,12 +1,22 @@
-use axum::{routing::*};
+use axum::{routing::*, Json};
+use serde_json::{json, Value};
 
 use crate::api::parses::post_new_parse_handler;
 
 mod parses;
 
+async fn get_health_check() -> Json<Value> { 
+    return Json(json!({ "app": "payobills.transaction-parser"}))
+}
+
 pub(crate) fn create_router() -> Router {
-    Router::new().merge(Router::new().route(
+    return Router::new()
+    .route(
+        "/",
+        get(get_health_check),
+    )
+    .route(
         "/api/parses/:transaction_id",
         post(move |transaction_id| post_new_parse_handler(transaction_id)),
-    ))
+    )
 }
