9a18f61ce6b306178f2ec232c25bd7d5ee103165
mrsauravsahu <mrsauravsahu@outlook.com>
2025-08-17 16:06:12 +0530

    wip: setup UI to be built with static svelte kit adapter

---
+++ apps/ui/src/vite.config.static.ts
@@ -0,0 +1,22 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteInspector } from '@sveltejs/vite-plugin-svelte-inspector';
+import type { UserConfig } from 'vite';
+
+const config: UserConfig = {
+	plugins: [sveltekit(), svelteInspector({})],
+	optimizeDeps: {
+		exclude: ['@urql/svelte'],
+	},
+	server: {
+		proxy: {
+			// https://stackoverflow.com/questions/64677212/how-to-configure-proxy-in-vite
+			'/gateway/graphql': `${process.env.GATEWAY}/graphql`,
+			'/files': {
+				target: process.env.FILES_SERVICE,
+				rewrite: (path) => path.replace(/^\/files/, ''),
+			},
+		}
+	}
+};
+
+export default config;
