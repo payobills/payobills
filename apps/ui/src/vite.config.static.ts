8e59e1f3dc067ecc6105d49f327d4a2dda0a9cff
Saurav Sahu <mrsauravsahu@outlook.com>
2026-02-11 20:37:04 +0530

    Improv(UI): Use dark mode by default and setup tailwindcss for static build (#481)
    chore: use dark mode by default and setup tailwindcss for static building

--- apps/ui/src/vite.config.static.ts
+++ apps/ui/src/vite.config.static.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { svelteInspector } from '@sveltejs/vite-plugin-svelte-inspector';
+import tailwindcss from "@tailwindcss/vite";
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit(), svelteInspector({})],
+	plugins: [tailwindcss(), sveltekit(), svelteInspector({})],
 	optimizeDeps: {
 		exclude: ['@urql/svelte'],
 	},
